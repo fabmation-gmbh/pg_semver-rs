dc46d4dbc44d019de95d9c8fe5d251527ae542f6
Emanuel Bennici <eb@fabmation.de>
2023-01-25 17:30:11 +0100

    Implement operators for semver

--- src/version.rs
+++ src/version.rs
@@ -1,11 +1,16 @@
+use std::cmp::Ordering;
+use std::hash::{Hash, Hasher};
+
 use pgx::{cstr_core::CStr, StringInfo};
-use pgx::error;
+use pgx::{error, PostgresHash, PostgresOrd, PostgresEq};
 use pgx::prelude::*;
+use pgx::*;
 use pgx::inoutfuncs::InOutFuncs;
 use semver::Version;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, PostgresType)]
+#[derive(PostgresEq, PostgresOrd, PostgresHash)]
 #[inoutfuncs]
 pub struct Semver {
     version: Version
@@ -30,6 +35,31 @@ impl InOutFuncs for Semver {
     }
 }
 
+impl PartialEq for Semver {
+    fn eq(&self, other: &Self) -> bool {
+        self.version == other.version
+    }
+}
+
+impl Eq for Semver {}
+
+impl Ord for Semver {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.version.cmp(&other.version)
+    }
+}
+
+impl PartialOrd for Semver {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Hash for Semver {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.version.hash(state);
+    }
+}
 
 #[pg_extern]
 fn to_semver(version: &str) -> Semver {
