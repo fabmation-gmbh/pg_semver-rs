828ed6cc285f1faa82781ad6fe076929f96736d5
Emanuel Bennici <eb@fabmation.de>
2023-01-25 16:58:55 +0100

    Implement custom Semver representation

--- src/version.rs
+++ src/version.rs
@@ -1,10 +1,29 @@
+use pgx::{cstr_core::CStr, StringInfo};
 use pgx::prelude::*;
+use pgx::inoutfuncs::InOutFuncs;
 use semver::Version;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, PostgresType)]
+#[inoutfuncs]
 pub struct Semver {
     version: Version
+}
+
+impl InOutFuncs for Semver {
+    fn input(input: &CStr) -> Semver {
+        let input_str = match input.to_str() {
+            Ok(data) => data,
+            Err(error) => panic!("Unable to get &str representation from &CStr: {:?}", error),
+        };
+        let version = Version::parse(input_str).unwrap();
+
+        Semver { version }
+    }
+
+    fn output(&self, buffer: &mut StringInfo) {
+        buffer.push_str(&format!("{}", self.version));
+    }
 }
 
 
